941e9993ef11e7c8180950dbb43c9d164c30afb4
Kayvon <kayvonwallace@gmail.com>
2024-10-02 14:46:48 -0700

    I've added in middleware to handle the validate fo r for adding and retriving notes. I've also added error handling in the validation so it can pass an  stutus code and a message.

--- src/app.js
+++ src/app.js
@@ -3,21 +3,70 @@ const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 const pastes = require("./data/pastes-data");
 
-app.use("/pastes", (req, res) => {
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next({ status: 404 , message: `Paste id not found: ${pasteId}`});
+  }
+});
+
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
 
+// New middleware function to validate the request body
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required.",
+  });
+}
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post(
+  "/pastes", 
+  bodyHasTextProperty,
+   (req, res, next) => {
+    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+    const newPaste = {
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+});
+
 // Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
